33df8336ad09e17b9020a4be4b51e1b3e6bbe135
chezynick <chezynickannie@yahoo.co.uk>
2023-12-25 13:06:22 +0000

    added train and carraige;

--- src/utils/colours/index.jsx
+++
@@ -1,8 +0,0 @@
-const colours = [
-    {id: 0, bg: 'bg-red-500'},
-    {id: 1, bg: 'bg-yellow-500'},
-    {id: 2, bg: 'bg-blue-500'},
-    {id: 3, bg: 'bg-green-500'},
-    {id: 4, bg: 'bg-black-500'},
-];
-export default colours;
