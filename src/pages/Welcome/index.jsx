e66e90375a46c6a82ba24644e20bd4e1451ebf9a
chezynick <chezynickannie@yahoo.co.uk>
2023-12-25 17:52:50 +0000

    updated traiun

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import Carraige from "../../components/Carraige";
+import Engine from "../../components/Engine";
 
 const Welcome = ({setPage}) => {
     const [startTrain, setStartTrain] = useState(false)
   const handleClick = ()=>{
         setTimeout(()=> setStartTrain(true),100)
-        setTimeout(()=>setPage(1),1100)
+        setTimeout(()=>setPage(1),1500)
     }
     return (
         <div className="relative flex items-center justify-center w-full h-full">
-            <div className={`absolute  top-10 h-4 transition-all duration-[1500ms] ease-in-out flex  bg-red-400 ${startTrain ? 'left-[105%]' : '-left-[50px]'}`}>train</div>
+            <div className={`absolute  top-10 h-4 transition-all duration-[1800ms] ease-in-out flex ${startTrain ? 'left-[105%]' : '-left-[150px]'}`}>
+           <div className="flex items-end justify-center h-10"> <Carraige /><Carraige /><Engine /></div>
+            </div>
             <button onClick={()=>handleClick()} className="text-xl text-gray-300">Start game</button>
         </div>
     )
